6f9a5d835f044ec65864d6eb43a2e12246bcc310
jakob strate <jastr23@student.sdu.dk>
2025-05-18 14:25:55 +0200

    made test work probably

--- Collision/src/main/java/dk/cbse/jakob/collisionsystem/CollisionDetector.java
+++ Collision/src/main/java/dk/cbse/jakob/collisionsystem/CollisionDetector.java
@@ -12,37 +12,40 @@ import org.springframework.stereotype.Component;
 import java.util.ServiceLoader;
 
 @Component
-public class CollisionDetector implements IPostEntityProcessingService{
-    private final IAsteroidSplitter splitter = ServiceLoader.load(IAsteroidSplitter.class)
-            .findFirst().orElseThrow(()-> new IllegalStateException("cant find splitter"));
+public class CollisionDetector implements IPostEntityProcessingService {
+
+    private final IAsteroidSplitter splitter;
+
+    public CollisionDetector() {
+        this(ServiceLoader.load(IAsteroidSplitter.class)
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("cant find splitter")));
+    }
+
+    // Constructor for injecting a mock or custom splitter (used in testing)
+    public CollisionDetector(IAsteroidSplitter splitter) {
+        this.splitter = splitter;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 
     @Override
     public void process(GameData gameData, World world) {
-        // two for loops for all entities in the world
         for (Entity entity1 : world.getEntities()) {
             for (Entity entity2 : world.getEntities()) {
+                if (entity1.getID().equals(entity2.getID())) continue;
+                if (entity1.getClass().equals(Asteroid.class) && entity2.getClass().equals(Asteroid.class)) continue;
 
-                // if the two entities are identical, skip the iteration
-                if (entity1.getID().equals(entity2.getID())) {
-                    continue;
-                }
-                if (entity1.getClass().equals(Asteroid.class) && entity2.getClass().equals(Asteroid.class)){
-                    continue;
-                }
-
-                // CollisionDetection
                 if (this.collides(entity1, entity2)) {
-                    if (entity1.getClass().equals(Asteroid.class) && entity2.getClass().equals(Bullet.class)){
+                    if (entity1 instanceof Asteroid && entity2 instanceof Bullet) {
                         world.removeEntity(entity2);
-                        splitter.createSplitAsteroid(entity1,world);
-                    }
-                    if (entity1.getClass().equals(Bullet.class) && entity2.getClass().equals(Asteroid.class)){
+                        splitter.createSplitAsteroid(entity1, world);
+                    } else if (entity1 instanceof Bullet && entity2 instanceof Asteroid) {
                         world.removeEntity(entity1);
-                        splitter.createSplitAsteroid(entity2,world);
-                    }else {
+                        splitter.createSplitAsteroid(entity2, world);
+                    } else {
                         world.removeEntity(entity1);
                         world.removeEntity(entity2);
                         entity1.onCollision(world, gameData);
@@ -51,7 +54,6 @@ public class CollisionDetector implements IPostEntityProcessingService{
                 }
             }
         }
-
     }
 
     public Boolean collides(Entity entity1, Entity entity2) {
@@ -60,5 +62,4 @@ public class CollisionDetector implements IPostEntityProcessingService{
         float distance = (float) Math.sqrt(dx * dx + dy * dy);
         return distance < (entity1.getRadius() + entity2.getRadius());
     }
-
 }
